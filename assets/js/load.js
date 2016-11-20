92e73627ce0424896cd0180482d5b796a0269aba
Lokhendro <Lokhendro@gmail.com>
2016-11-21 03:32:26 +0530

    Clean Up
    Clean Up

--- assets/js/load.js
+++ assets/js/load.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+    $("#markdown-toc").detach().appendTo('.toc-sidebar');
+    //$('.toc-sidebar').attr({"class":"nav nav-pills nav-stacked"});
+    $('#markdown-toc').attr({"class":"nav navbar-nav"});
     $('.post-content h2').prepend('<span class="glyphicon glyphicon-grain"></span> ');
     $('.post-content h3').prepend('<span class="glyphicon glyphicon-hand-right"></span> ');
 });

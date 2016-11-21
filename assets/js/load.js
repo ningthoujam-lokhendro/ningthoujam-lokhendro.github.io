3af1cb387304bf155dd3d3374831c791aaa3d517
Lokhendro <Lokhendro@gmail.com>
2016-11-21 23:29:15 +0530

    add sidebar
    add sidebar

--- assets/js/load.js
+++ assets/js/load.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
     $("#markdown-toc").detach().appendTo('.toc-sidebar');
     //$('.toc-sidebar').attr({"class":"nav nav-pills nav-stacked"});
-    $('#markdown-toc').attr({"class":"nav navbar-nav"});
+    //$('#markdown-toc').attr({"class":"nav navbar-nav"});
     $('.post-content h2').prepend('<span class="glyphicon glyphicon-grain"></span> ');
     $('.post-content h3').prepend('<span class="glyphicon glyphicon-hand-right"></span> ');
+    $('#toc').BootSideMenu({
+        side: "left",
+        width: "30%"
+    });
 });
